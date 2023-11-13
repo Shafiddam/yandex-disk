e470cb91696012621f5b283281cd68514e4788df
fidail_shafikov <fidail_shafikov@enot.work>
2023-11-13 13:28:08 +0300

    new folder

---
+++ settings/settings.py
@@ -0,0 +1,10 @@
+BASE_URL_GET = 'https://cloud-api.yandex.net/v1'
+URL_GET_DISK_RESOURCES = f'{BASE_URL_GET}/disk/resources'
+URL_GET_DISK = f'{BASE_URL_GET}/disk/'
+URL_GET_DISK_RESOURCES_FILES = f'{BASE_URL_GET}/disk/resources/files'
+HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'OAuth y0_AgAAAAByGtlWAArOsAAAAADx21niZDZqjYszSzOjo7NxgkgvCv4DK2A'
+}
+PATH = '/'
